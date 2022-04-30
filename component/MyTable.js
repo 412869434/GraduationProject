eda4ca1eea828f40403b2b1aa06d0f059ea8e43c
412869434 <1149941442@qq.com>
2022-04-30 11:14:29 +0800

    add antd

---
+++ component/MyTable.js
@@ -0,0 +1,73 @@
+import { Table, Tag } from "antd";
+import "antd/dist/antd.css";
+import { useState, useEffect } from "react";
+import info from "../public/info";
+
+const columns = [
+  {
+    title: "类别ID",
+    dataIndex: "ID",
+    key: "ID",
+  },
+  {
+    title: "藻类门",
+    dataIndex: "Phylum",
+    key: "Phylum",
+  },
+  {
+    title: "藻类属",
+    dataIndex: "Genus",
+    key: "Genus",
+  },
+  {
+    title: "藻类种",
+    dataIndex: "Species",
+    key: "Species",
+  },
+  {
+    title: "概率",
+    key: "score",
+    dataIndex: "score",
+    render: (score) => {
+      const color = Number(score) >= 0.8 ? "green" : "red";
+      return (
+        <>
+          <Tag color={color} key={score}>
+            {score}
+          </Tag>
+        </>
+      );
+    },
+  },
+];
+
+function MyTable(props) {
+  const { data } = props;
+
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const tmp = data.map((item) => {
+      const algae = info.find((i) => i.ID === item.label);
+      const { ID, Phylum, Genus, Species } = algae;
+      const key = ID + item.label + item.x;
+      return {
+        key: key,
+        Phylum: Phylum,
+        Genus: Genus,
+        Species: Species,
+        ID: item.label,
+        score: item.score,
+      };
+    });
+    setTableData(tmp);
+  }, [data]);
+
+  return (
+    <>
+      <Table columns={columns} dataSource={tableData} pagination={{ pageSize: 5 }} />
+    </>
+  );
+}
+
+export default MyTable;
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,12 @@
-import {
-  Center,
-  Flex,
-  Text,
-  VisuallyHiddenInput,
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-  TableContainer,
-} from "@chakra-ui/react";
+import { Center, Flex, Text, VisuallyHiddenInput, Heading } from "@chakra-ui/react";
 import * as tf from "@tensorflow/tfjs";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import PreviewContainer from "../component/PreviewContainer";
 import PromptContainer from "../component/PromptConainer";
+import info from "../public/info";
+import ReactECharts from "echarts-for-react";
+import MyTable from "../component/MyTable";
 
 const BG_UPLOAD = `url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg'%3E%3Crect width='100%25' height='100%25' fill='none' stroke='%23E4E6ED' stroke-width='4' stroke-dasharray='4, 12' stroke-linecap='square'/%3E%3C/svg%3E")`;
 const WEIGHTS = "best_web_model/model.json";
@@ -26,6 +16,27 @@ export default function Home() {
   const [isModelLoaded, setModelLoaded] = useState();
   const [isHover, setHover] = useState(false);
   const [data, setData] = useState([]);
+
+  const options = {
+    grid: { top: 8, right: 8, bottom: 24, left: 36 },
+    xAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: "line",
+        smooth: true,
+      },
+    ],
+    tooltip: {
+      trigger: "axis",
+    },
+  };
 
   useEffect(() => {
     console.log(data);
@@ -63,12 +74,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Center as="main" h="100vh">
-        <Flex className="object-detection" justifyContent="center" alignItems="flex-start" marginTop="-10em">
+      <Flex justifyContent="center" marginTop="1em">
+        <Heading>藻类识别计数系统</Heading>
+      </Flex>
+
+      <Center as="main" h="100vh" marginTop="-10em">
+        <Flex className="object-detection" justifyContent="center" alignItems="flex-start">
           <Flex
             className="detection-container"
-            justifyContent="center"
-            alignItems="flex-start"
             padding="1em"
             cursor="pointer"
             position="relative"
@@ -88,38 +101,13 @@ export default function Home() {
             )}
           </Flex>
           <VisuallyHiddenInput id="file-input" type="file" accept="image/*" onChange={(e) => uploadHandler(e)} />
-          <Flex marginLeft="3em" marginTop="-3em">
-            <TableContainer>
-              <Table variant="simple">
-                <TableCaption placement="top">藻类识别统计表格</TableCaption>
-                <Thead>
-                  <Tr>
-                    <Th>藻类门</Th>
-                    <Th>藻类属</Th>
-                    <Th>藻类种</Th>
-                    <Th>概率</Th>
-                    <Th isNumeric>数量</Th>
-                  </Tr>
-                </Thead>
-
-                <Tbody>
-                  {data.map((item) => (
-                    <>
-                      <Tr key={item.label + item.score + item.x}>
-                        <Td>{item.label}</Td>
-                        <Td>{item.label}</Td>
-                        <Td>{item.label}</Td>
-                        <Td>{item.score}</Td>
-                        <Td isNumeric>1</Td>
-                      </Tr>
-                    </>
-                  ))}
-                </Tbody>
-              </Table>
-            </TableContainer>
+          <Flex marginLeft="3em">
+            <MyTable data={data} />
           </Flex>
         </Flex>
       </Center>
+
+      <ReactECharts option={options} />
     </>
   );
 }
